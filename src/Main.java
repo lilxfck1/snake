755041e08845720312ee7f020591663164b4b053
dcarriochi <dcarriochi@educacion.navarra.es>
2025-12-16 12:43:46 +0100

    Cambios de Main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,35 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
+    Mapa mapa = new Mapa(10, 10);
+
+        System.out.println("*".repeat(45));
+        System.out.println("¡BIENVENIDO AL MENU DEL JUEGO DE LA SERPIENTE!");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("1. Jugar");
+        System.out.println("2. Puntuacion");
+        System.out.println("0. Salir");
+        System.out.println("*".repeat(45));
+
+        System.out.print("* Eligue una opocion: ");
+        int opcion = Integer.parseInt(sc.nextLine());
+
+        switch (opcion){
+            case 1:
+            if (opcion == 1){
+                mapa.iniciarJuego();
+            }
+            case 2:
+            if (opcion == 2){
+                mapa.verPuntuacion();
+            }
+            case 3:
+            if (opcion == 3){
+                mapa.salir();
+            }
+        }
     }
 }
