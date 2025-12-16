e8ed69ad93071881f7d219e137f602a249cfe9bb
lilxfck1 <107131710+lilxfck1@users.noreply.github.com>
2025-12-16 13:20:25 +0100

    mover serpiente

--- src/Mapa.java
+++ src/Mapa.java
@@ -28,15 +28,20 @@ public class Mapa {
     }
 
     public void iniciarJuego() {
-        tamano[0][0] = serpiente;
+        int posicion = 0;
         while (vivo) {
             try {
+                posicion++;
+                tamano[0][posicion] = serpiente;
+                if (posicion >+ 1) {
+                    tamano[0][posicion-1] = "0";
+                }
                 Thread.sleep(850);
                 for (int i = 0; i < 20; i++) {
                     System.out.println();
                 }
                 for (int i = 0; i < tamano.length; i++) {
-                    for (int j = 0; j < tamano[0].length; j++) {
+                    for (int j = 0; j < tamano[0].length - 1; j++) {
                         System.out.print(tamano[i][j]);
                     }
                     System.out.println();
@@ -44,6 +49,8 @@ public class Mapa {
             } catch (InterruptedException e) {
                 Thread.currentThread().interrupt();
             }
+
+
         }
 
     }
