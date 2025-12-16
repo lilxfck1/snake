809402915180ca75849fcc6f402c220dd8ce5e0f
lilxfck1 <107131710+lilxfck1@users.noreply.github.com>
2025-12-16 13:01:17 +0100

    Update Mapa.java

--- src/Mapa.java
+++ src/Mapa.java
@@ -1,4 +1,50 @@
-package PACKAGE_NAME;
+import java.util.concurrent.TimeUnit;
+import java.util.Scanner;
 
 public class Mapa {
+    int x;
+    int y;
+    String tamano[][];
+    boolean vivo = true;
+    Scanner sc = new Scanner(System.in);
+    String serpiente = "\uD83D\uDCA2";
+
+    public Mapa(int x, int y) {
+        this.x = x;
+        this.y = y;
+        this.tamano = new String[x][y];
+
+        for (int i = 0; i < tamano.length; i++) {
+            for (int j = 0; j < tamano[0].length; j++) {
+                tamano[i][j] = "0";
+            }
+        }
+    }
+
+    public void moverSerpiente(String opcion) {
+        if (opcion.equalsIgnoreCase("d")) {
+
+        }
+    }
+
+    public void iniciarJuego() {
+        tamano[0][0] = serpiente;
+        while (vivo) {
+            try {
+                Thread.sleep(850);
+                for (int i = 0; i < 20; i++) {
+                    System.out.println();
+                }
+                for (int i = 0; i < tamano.length; i++) {
+                    for (int j = 0; j < tamano[0].length; j++) {
+                        System.out.print(tamano[i][j]);
+                    }
+                    System.out.println();
+                }
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        }
+
+    }
 }
